a0527dce5eacaf1b0cc2a99dc84b9ba702bb898e
August Solli Middelkoop <august.kampen@gmail.com>
2025-11-10 18:58:52 +0100

    Update

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   name: 'default',
   title: 'new-norstec-website',
 
-  projectId: process.env.NEXT_PROJECT_ID!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
 
   plugins: [structureTool(), visionTool()],
