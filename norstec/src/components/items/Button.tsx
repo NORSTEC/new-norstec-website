3600daa919b3f4a0a823321013ffe6a0f5059a2f
eirikek <eirik.engen.kvam@gmail.com>
2025-11-25 21:56:06 +0100

    Add Button and MapFilter components; integrate filtering functionality in SectionMap

---
+++ norstec/src/components/items/Button.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import React from "react";
+
+type ButtonPillProps = {
+    active: boolean;
+    children: React.ReactNode;
+    onClick: () => void;
+};
+
+export default function Button({ active, children, onClick }: ButtonPillProps) {
+    return (
+        <button
+            type="button"
+            onClick={onClick}
+            className={
+                "rounded-full px-4 py-1 transition-all border-2 border-moody cursor-pointer " +
+                (active
+                    ? "bg-moody text-egg"
+                    : "hover:bg-moody hover:text-egg")
+            }
+        >
+            {children}
+        </button>
+    );
+}
--- norstec/src/components/sections/SectionMap.tsx
+++ norstec/src/components/sections/SectionMap.tsx
@@ -1,13 +1,37 @@
-import Map from "@/components/items/Map";
+"use client"
+
+import Map from "@/components/items/map/Map";
 import type { SectionMap as SectionMapType } from "@/types/sections/sectionMap";
 import {PortableText} from "next-sanity";
 import StripesVertical from "@/components/items/stripes/StripesVertical";
+import {useMemo, useState} from "react";
+import MapFilter, {MapFilterId} from "@/components/items/map/MapFilter";
+import {MOCK_ORGANIZATIONS} from "@/mock/organizations";
 
 type SectionMapProps = {
     section: SectionMapType;
 };
 
 export default function SectionMap({ section }: SectionMapProps) {
+    //TODO Bytter til denne når satt opp sanity schemas
+    const allOrgs = useMemo(
+        () => section.organizations ?? [],
+        [section.organizations]
+    );
+
+    const [filter, setFilter] = useState<MapFilterId>("all");
+
+    //TODO Gjøre om etter å ha satt opp shcemas
+    const rawOrgs =
+        section.organizations && section.organizations.length > 0
+            ? section.organizations
+            : MOCK_ORGANIZATIONS;
+
+    const filteredOrgs = useMemo(() => {
+        if (filter === "all") return rawOrgs;
+        return rawOrgs.filter((o) => o.type === filter);
+    }, [rawOrgs, filter]);
+
     return (
         <section className="section md:px-[40px]">
             <StripesVertical
@@ -15,15 +39,23 @@ export default function SectionMap({ section }: SectionMapProps) {
                 side="right"
             />
 
-            <div className="flex flex-col lg:flex-row h-full w-full justify-between gap-10">
-                <div className="w-[clamp(30rem,50vw,40rem)] md:py-10 my-auto max-w-screen mx-auto">
-                    <Map organizations={section.organizations} />
+            <h2 className="block lg:hidden text-h2 italic">
+                {section.title}
+            </h2>
+            <div className="flex flex-col lg:flex-row h-full w-full justify-between lg:gap-10">
+                <div className="w-[clamp(30rem,50vw,40rem)] md:py-10 my-auto max-w-screen mx-auto md:mx-0">
+                    <Map organizations={filteredOrgs} />
                 </div>
+                <MapFilter
+                    value={filter}
+                    onChange={setFilter}
+                    className="inline-flex lg:hidden"
+                />
 
 
                 <aside className="flex flex-col lg:w-[clamp(50rem,50vw,80rem)]">
                     <div className="bg-egg md:pt-10 md:pb-5 flex md:justify-end">
-                        <h2 className="text-h2 italic">
+                        <h2 className="hidden lg:block text-h2 italic">
                             {section.title}
                         </h2>
                     </div>
@@ -41,6 +73,11 @@ export default function SectionMap({ section }: SectionMapProps) {
                             }}
                         />
                     </div>
+                    <MapFilter
+                        value={filter}
+                        onChange={setFilter}
+                        className="hidden lg:inline-flex mt-2 md:mt-4"
+                    />
                 </aside>
             </div>
         </section>
