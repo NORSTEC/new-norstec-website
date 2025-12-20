a7260f45fb4e3d2e2a2df7b7c5aae4f73b695f53
eirikek <eirik.engen.kvam@gmail.com>
2025-12-20 22:19:32 +0100

    Refactor SectionTextImage component; update layout and styles for improved responsiveness

--- norstec/src/components/sections/SectionTextImage.tsx
+++ norstec/src/components/sections/SectionTextImage.tsx
@@ -11,17 +11,17 @@ export default function SectionTextImage2({ section }: SectionTextImageProps) {
     const { title, body, images, mirrored } = section;
 
     return (
-        <section className="section relative lg:h-auto! xl:h-screen! mobile-container">
+        <section className="section relative mobile-container">
 
             <StripesVertical
                 side="left"
             />
-            <div className="flex flex-col justify-center h-full stripes-left gap-10">
+            <div className="flex flex-col justify-center h-full stripes-left lg:gap-10">
 
                 <div>
-                    {title && <h2 className="text-h2 italic pb-5">{title}</h2>}
+                    {title && <h2 className="order-1 text-h2 italic pb-5">{title}</h2>}
 
-                    <div className="order-2 md:order-1 md:py-0">
+                    <div className="hidden lg:block lg:py-0">
                         <PortableText
                             value={body}
                             components={{
@@ -37,8 +37,22 @@ export default function SectionTextImage2({ section }: SectionTextImageProps) {
                     </div>
                 </div>
 
-                <div className="md:flex-1 flex items-center h-full order-1 md:order-2">
+                <div className="md:flex-1 flex items-center h-full pb-5 lg:pb-0">
                     <ImageCarousel images={images} className="w-full" />
+                </div>
+                <div className="block lg:hidden lg:py-0">
+                    <PortableText
+                        value={body}
+                        components={{
+                            block: {
+                                normal: ({ children }) => (
+                                    <p className="mb-[1rem] last:mb-0">
+                                        {children}
+                                    </p>
+                                ),
+                            },
+                        }}
+                    />
                 </div>
             </div>
 
