51eab3b00c4a90fb693d5ad44c0255206edf6456
August Solli Middelkoop <august.kampen@gmail.com>
2025-12-23 19:04:27 +0100

    Test for summit countdown as link

--- norstec/src/app/summit/page.tsx
+++ norstec/src/app/summit/page.tsx
@@ -25,7 +25,7 @@ export async function generateMetadata(): Promise<Metadata> {
             title,
             images: [
                 {
-                    url: `/og/summit?days=${days}`,
+                    url: '/images/summitTest.png',
                     width: 1200,
                     height: 630,
                 },
@@ -35,7 +35,7 @@ export async function generateMetadata(): Promise<Metadata> {
         twitter: {
             card: "summary_large_image",
             title,
-            images: [`/og/summit?days=${days}`],
+            images: ['/images/summitTest.png'],
         },
     };
 }
