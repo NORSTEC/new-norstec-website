308194300294cea48e31ec7e10360808359c42c3
August Solli Middelkoop <august.kampen@gmail.com>
2025-12-23 11:19:23 +0100

    Short revalidate, temp solution until webhook is ready.

--- norstec/src/app/page.tsx
+++ norstec/src/app/page.tsx
@@ -1,6 +1,7 @@
 import {getHomePage, getInitiativesPage} from "@/sanity/fetch/SanityFetch";
 import ClientHomePage from "@/app/ClientHomePage";
 
+export const revalidate = 5; // midlertidig, skal sette opp webhooks.
 
 export default async function HomePage() {
   const homePage = await getHomePage();
