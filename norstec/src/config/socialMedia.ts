c91c34e14f93316fa6dba6b14bc9757c8c12d208
Magnus Giverin <magnusgiverin@icloud.com>
2026-01-08 10:41:07 +0700

    Add: add footer

---
+++ norstec/src/config/socialMedia.ts
@@ -0,0 +1,10 @@
+export type SocialMediaItem = {
+  label: string;
+  href: string;
+};
+
+export const SOCIAL_MEDIA: SocialMediaItem[] = [
+    { label: "Instagram", href: "https://www.instagram.com/norstec/" },
+    { label: "LinkedIn", href: "https://www.linkedin.com/company/norstec/" },
+    { label: "YouTube", href: "https://www.youtube.com/@NORSTEC-NO" },
+];
