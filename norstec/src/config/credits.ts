8aef1c242991cc44fe2a39013b72868114cd9a44
eirikek <eirik.engen.kvam@gmail.com>
2026-01-20 20:40:50 +0100

    Refactor credits and Footer components for improved accessibility and styling

--- norstec/src/config/credits.ts
+++ norstec/src/config/credits.ts
@@ -5,7 +5,7 @@ export type CreditItem = {
 };
 
 export const CREDITS: CreditItem[] = [
-  { name: "A.S. Middelkoop", accreditation: "Developer & Design", href: "https://www.linkedin.com/in/august-solli-middelkoop/?originalSubdomain=no" },
-  { name: "E.E. Kvam", accreditation: "Developer & Design", href: "https://www.linkedin.com/in/eirik-engen-kvam/" },
-    { name: "M.A. Giverin", accreditation: "Developer & Design", href: "https://www.linkedin.com/in/magnus-giverin-5344b5188/" },
+  { name: "August Middelkoop", accreditation: "Developers & Design", href: "https://www.linkedin.com/in/august-solli-middelkoop/?originalSubdomain=no" },
+  { name: "Eirik Kvam", accreditation: "Developer & Design", href: "https://www.linkedin.com/in/eirik-engen-kvam/" },
+    { name: "Magnus Giverin", accreditation: "Developer & Design", href: "https://www.linkedin.com/in/magnus-giverin-5344b5188/" },
 ];
