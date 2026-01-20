7b2f6c2e12969526f8c3de61a333ecb9a5016321
eirikek <eirik.engen.kvam@gmail.com>
2026-01-20 18:39:11 +0100

    Refactor SectionHero component to conditionally render mobile and desktop images

--- norstec/src/components/sections/SectionHero.tsx
+++ norstec/src/components/sections/SectionHero.tsx
@@ -24,7 +24,9 @@ export default function SectionHero({ section, className = "" }: SectionHeroProp
         fit: "max",
     });
 
-  const TextOverlay = (
+    const hasMobileImage = Boolean(imageMobile);
+
+    const TextOverlay = (
     <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center text-egg-static drop-shadow-[0_2px_16px_rgba(0,0,0,0.65)]">
       <h1 className="text-[2rem] sm:text-[3.5rem] md:text-[4.5rem] xl:text-[7rem] 2xl:text-[9rem] font-light font-barlow uppercase flex items-center justify-center">
         {title}
@@ -43,18 +45,28 @@ export default function SectionHero({ section, className = "" }: SectionHeroProp
     return (
       <section className={`section mobile-container p-0!  ${className}`}>
         <div className="relative w-full overflow-hidden">
-
           <div className="relative w-screen h-screen overflow-hidden">
+            {hasMobileImage && (
               <NextImage
-                src={desktopSrc}
+                src={mobileSrc}
                 alt={title}
                 fill
                 sizes="100vw"
-                className="object-cover"
+                className="object-cover block md:hidden"
                 priority
                 unoptimized
               />
-            <div className="absolute inset-0 z-5 bg-linear-to-b from-black/90 via-black/70 to-black/20" />
+            )}
+
+            <NextImage
+              src={desktopSrc}
+              alt={title}
+              fill
+              sizes="100vw"
+              className={`object-cover ${hasMobileImage ? "hidden md:block" : "block"}`}
+              priority
+              unoptimized
+            />
 
             {TextOverlay}
           </div>
