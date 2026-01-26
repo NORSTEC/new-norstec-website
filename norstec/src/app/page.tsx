6739bf1eaa5a0f0e7bf13e187d7a28c2ef4ce8c6
August Solli Middelkoop <august.kampen@gmail.com>
2026-01-26 18:29:50 +0100

    Metadata team

--- norstec/src/app/page.tsx
+++ norstec/src/app/page.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
 
   openGraph: {
     type: "website",
-    title: "NORSTEC",
+    title: "Securing our future in space | NORSTEC",
     description: "NORSTEC supports student space projects across Norway.",
     url: "https://norstec.no",
     images: [
@@ -34,7 +34,7 @@ export const metadata: Metadata = {
 
   twitter: {
     card: "summary_large_image",
-    title: "NORSTEC",
+    title: "Securing our future in space | NORSTEC",
     description: "NORSTEC supports student space projects across Norway.",
     images: ["/landing.jpeg"],
   },
--- norstec/src/app/team/page.tsx
+++ norstec/src/app/team/page.tsx
@@ -1,7 +1,41 @@
 import ClientTeamPage from "@/app/team/ClientTeamPage";
 import { getTeamPage } from "@/sanity/fetch/SanityFetch";
+import type {Metadata} from "next";
 
 export const dynamic = 'force-dynamic'
+export const metadata: Metadata = {
+  title: "Where Space Careers Launch | TEAM",
+  description: "NORSTEC supports student space projects across Norway.",
+
+  metadataBase: new URL("https://norstec.no"),
+
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  openGraph: {
+    type: "website",
+    title: "Where Space Careers Launch | TEAM",
+    description: "NORSTEC supports student space projects across Norway.",
+    url: "https://norstec.no",
+    images: [
+      {
+        url: "/team.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "NORSTEC",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Where Space Careers Launch | TEAM",
+    description: "NORSTEC supports student space projects across Norway.",
+    images: ["/team.jpeg"],
+  },
+};
 
 export default async function TeamPage() {
   const teamPage = await getTeamPage();
