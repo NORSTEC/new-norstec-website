8e1355461e5db7647310c07602f75e473d07f557
August Solli Middelkoop <august.kampen@gmail.com>
2026-01-26 18:37:03 +0100

    Metadata about

--- norstec/src/app/about/page.tsx
+++ norstec/src/app/about/page.tsx
@@ -1,6 +1,45 @@
 import { getAboutPage } from "@/sanity/fetch/SanityFetch";
 import ClientAboutPage from "@/app/about/ClientAboutPage";
 export const dynamic = 'force-dynamic'
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Our Story | NORSTEC",
+  description:
+      "Learn about NORSTEC’s mission to support student-led space projects in Norway, develop future space professionals, and strengthen the national space ecosystem.",
+
+  metadataBase: new URL("https://norstec.no"),
+
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  openGraph: {
+    type: "website",
+    title: "Our Story | NORSTEC",
+    description:
+        "NORSTEC supports student-led space projects and develops the next generation of space professionals in Norway.",
+    url: "https://norstec.no/about",
+    images: [
+      {
+        url: "/images/about.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Our Story | NORSTEC",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "About NORSTEC",
+    description:
+        "Learn about NORSTEC’s mission to support student space projects in Norway.",
+    images: ["/images/about.jpeg"],
+  },
+};
+
 export default async function AboutPage() {
   const aboutPage = await getAboutPage();
 
